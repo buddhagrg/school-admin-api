672871c30ce8a31cafdfe8afb9410e1977edfc0a
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2024-09-29 12:41:36 +0545

    use resend instead of nodemailer

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,9 @@
 const { corsPolicy } = require("./cors");
 const { db } = require("./db");
 const { env } = require("./env");
-const { transporter } = require("./mailer");
 
 module.exports = {
-    cors: corsPolicy,
-    db,
-    env,
-    transporter
+  cors: corsPolicy,
+  db,
+  env,
 };
