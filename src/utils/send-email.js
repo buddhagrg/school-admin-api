d9e7733003616d1e751f4fba573fe75667f9c956
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2024-09-29 13:24:27 +0545

    log for error

--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -6,6 +6,7 @@ const resend = new Resend(env.RESEND_API_KEY);
 const sendMail = async (mailOptions) => {
   const { error } = await resend.emails.send(mailOptions);
   if (error) {
+    console.log(error);
     throw new ApiError(500, "Unable to send email");
   }
 };
