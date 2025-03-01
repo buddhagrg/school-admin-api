f0a0f9e94825c95ad17488c34510ecec75fadeb4
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2025-03-01 22:46:17 +0545

    refactor code and bug fix

--- src/modules/academic-years/academic-year-repository.js
+++ src/modules/academic-years/academic-year-repository.js
@@ -1,7 +1,22 @@
 const processDBRequest = require("../../utils/process-db-request");
 
 const getAllAcademicYears = async (schoolId) => {
-  const query = `SELECT * FROM academic_years WHERE school_id = $1`;
+  const query = `
+    SELECT
+      t1.id,
+      t1.name,
+      t1.is_active AS "isActive",
+      t1.start_date AS "startDate",
+      t1.end_date AS "endDate",
+      t1.academic_level_id AS "academicLevelId",
+      t2.name AS "academicLevelName"
+    FROM academic_years t1
+    JOIN academic_levels t2 ON t2.id = t1.academic_level_id
+    WHERE t1.school_id = $1
+    ORDER BY
+      t1.is_active DESC,
+      t1.start_date DESC
+  `;
   const queryParams = [schoolId];
   const { rows } = await processDBRequest({ query, queryParams });
   return rows;
@@ -19,15 +34,30 @@ const addAcademicYear = async (payload) => {
 };
 
 const updateAcademicYear = async (payload) => {
-  const { schoolId, name, startDate, endDate, academicYearId } = payload;
+  const {
+    schoolId,
+    name,
+    startDate,
+    endDate,
+    academicYearId,
+    academicLevelId,
+  } = payload;
   const query = `
     UPDATE academic_years
     SET name = $1,
         start_date = $2,
-        end_date = $3
-    WHERE school_id = $4 AND id = $5
+        end_date = $3,
+        academic_level_id = $4
+    WHERE school_id = $5 AND id = $6
     `;
-  const queryParams = [name, startDate, endDate, schoolId, academicYearId];
+  const queryParams = [
+    name,
+    startDate,
+    endDate,
+    academicLevelId,
+    schoolId,
+    academicYearId,
+  ];
   const { rowCount } = await processDBRequest({ query, queryParams });
   return rowCount;
 };
@@ -40,7 +70,7 @@ const activateAcademicYear = async (payload) => {
       WHEN id = $1 THEN TRUE
       ELSE FALSE
     END
-    WHERE school_id = $1
+    WHERE school_id = $2
   `;
   const queryParams = [academicYearId, schoolId];
   const { rowCount } = await processDBRequest({ query, queryParams });
